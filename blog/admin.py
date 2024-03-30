a6e22da56478499b11c5debf59784c6b6c216cb4
mohsenkord <163458399+mohsenkord@users.noreply.github.com>
2024-03-30 05:17:52 +0330

    customize The admin panel for blog and website app models

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 from .models import Post, Tag, Category
 
+
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Tag)
-admin.site.register(Category)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_date', 'published_date')
+    list_filter = ('author', 'created_date', 'published_date')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('tag',)
+    date_hierarchy = 'published_date'
+    ordering = ('-published_date',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
--- website/admin.py
+++ website/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import ContactUs
+
+
 # Register your models here.
 
-admin.site.register(ContactUs)
+@admin.register(ContactUs)
+class ContactUsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'message', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('name', 'email', 'message')
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at',)
+    date_hierarchy = 'created_at'
+
+    def has_add_permission(self, request):
+        return False
