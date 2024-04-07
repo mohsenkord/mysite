e63a6c179c49b620e1f5fdaebd8c64a961791628
mohsenkord <163458399+mohsenkord@users.noreply.github.com>
2024-04-07 20:12:00 +0330

    add post-filter template and modify Necessary settings

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView
-
+from .views import BlogListView, BlogDetailView, post_filter, PostSearchListView
 
 app_name = 'blog'
 
+
 urlpatterns = [
     path('', BlogListView.as_view(), name='index'),
-    path('<int:pk>', BlogDetailView.as_view(), name='single-blog'),
+    path('search/', PostSearchListView.as_view(), name='blog-search'),
+    path('author/<slug:author_username>/', post_filter, name='post_filter_author'),
+    path('category/<slug:category_slug>/', post_filter, name='post_filter_category'),
+    path('tag/<slug:tag_slug>/', post_filter, name='post_filter_tag'),
+    path('<int:pk>/', BlogDetailView.as_view(), name='single-blog'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,55 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import Post
+from .models import Post, Category, Tag
 
 
 # Create your views here.
 class BlogListView(generic.ListView):
     template_name = 'blog/blog-home.html'
     queryset = Post.objects.filter(status='PU')
-    context_object_name = 'blog_list'
+    context_object_name = 'posts'
 
 
 class BlogDetailView(generic.DetailView):
     model = Post
     template_name = 'blog/blog-single.html'
     pk_url_kwarg = 'pk'
+
+
+def post_filter(request, category_slug=None, tag_slug=None, author_username=None):
+    global author
+    object_list = Post.objects.filter(status='PU')
+    category = None
+    tag = None
+
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        object_list = object_list.filter(categories=category)
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags=tag)
+
+    if author_username:
+        author = get_object_or_404(User, username=author_username)
+        object_list = object_list.filter(author__username=author_username)
+
+    return render(request,
+                  'blog/post-filter.html',
+                  {'category': category,
+                   'tag': tag,
+                   'author': author,
+                   'object_list': object_list})
+
+
+class PostSearchListView(generic.ListView):
+    model = Post
+    template_name = 'blog/post-search.html'
+
+    def get_queryset(self):
+        name = self.kwargs.get('q', '')
+        object_list = self.model.objects.filter(status='PU')
+        if name:
+            object_list = object_list.filter(name__icontains=name)
+        return object_list
